0b4f2b17e1d900c514ae803bba7bf9ff1ed71cc5
burresultan <brwasultankadir@gmail.com>
2022-05-31 17:55:07 +0200

    Fixed background image
    Co-Authored-By: simoneskilsson <81237644+simoneskilsson@users.noreply.github.com>

--- src/MovieWatch.js
+++ src/MovieWatch.js
@@ -63,20 +63,9 @@ export default function MovieWatch() {
 
     return (
         <div>
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Homepage</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Watchlist</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Movie History</a>
-                </li>
-            </ul>
             <div class="row mb-3" id='search-div'>
                 <div class="col-sm-12">
-                    <label for="search" class="col-sm-2 col-form-label">Search for a movie</label>
+                    <label for="search" class="col-sm-6 col-form-label">Search for a movie</label>
                     <input type="text" class="form-control" id="search" placeholder='Search for a title...' onChange={searchMovie} value={query}></input>
                 </div>
             </div>
