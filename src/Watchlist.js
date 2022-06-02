360eec01fe1b979d12c3b1c89bfa1d5035ce606b
burresultan <brwasultankadir@gmail.com>
2022-06-02 17:06:44 +0200

    Flyttat till app.js

--- src/Watchlist.js
+++ src/Watchlist.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import MovieList from './MovieList'
 
 export default function Watchlist() {
   return (
 
-    <div> <Nav></Nav>
-    
+    <div>
+
 
 
     </div>
